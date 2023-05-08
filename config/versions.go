19d9ac340a4b09d2e69267e5e1aba59e00089fec
Hal <haln_01@proton.me>
2023-05-07 23:57:07 -0700

    feat: config/version module

---
+++ config/versions.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Versions struct {
+	Database struct {
+		Banner string
+	}
+}
+
+func GetVersions() (*Versions, error) {
+	var buf Versions
+
+	f, err := os.ReadFile("./config/db/versions.yml")
+	if err != nil {
+		return nil, err
+	}
+
+	if err := yaml.Unmarshal(f, &buf); err != nil {
+		return nil, err
+	}
+
+	return &buf, nil
+}
--- go.mod
+++ go.mod
@@ -12,4 +12,5 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.15 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
