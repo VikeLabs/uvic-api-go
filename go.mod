dd1d599bf054f12b8874023f9ddae29d6f8079da
hal <haln_01@proton.me>
2023-05-09 11:43:45 -0700

    feat: cors allow all

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 )
 
 require (
+	github.com/go-chi/cors v1.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.15 // indirect
--- main.go
+++ main.go
@@ -7,10 +7,17 @@ import (
 	"github.com/VikeLabs/uvic-api-go/modules/ssf"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func main() {
 	mux := chi.NewMux()
+
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods: []string{"GET"},
+		AllowedHeaders: []string{"Accept", "Authorization", "Content-Type"},
+	}))
 
 	mux.Use(middleware.Recoverer)
 	mux.Use(middleware.Logger)
