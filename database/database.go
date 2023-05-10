587c40b607b96d6a8ab62974ea128f6f26217e60
Hal <haln_01@proton.me>
2023-05-09 22:46:37 -0700

    feat: db version control

--- database/database.go
+++ database/database.go
@@ -2,12 +2,36 @@ package database
 
 import (
 	"context"
+	"errors"
+	"log"
 	"os"
 	"strings"
 
+	"gopkg.in/yaml.v3"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
+
+type DbVersion struct {
+	StudySpaceFinder string `yaml:"ssf"`
+}
+
+var (
+	versionFile  string
+	databaseFile string
+)
+
+func init() {
+	databaseFile = strings.Join(
+		[]string{"database", "database.db"},
+		string(os.PathSeparator),
+	)
+
+	versionFile = strings.Join(
+		[]string{"database", "versions.yml"},
+		string(os.PathSeparator),
+	)
+}
 
 const (
 	Sections  string = "sections"
@@ -16,13 +40,47 @@ const (
 )
 
 func New(ctx context.Context) *gorm.DB {
-	path := strings.Join(
-		[]string{"database", "database.db"},
-		string(os.PathSeparator),
-	)
-	db, err := gorm.Open(sqlite.Open(path))
+	db, err := gorm.Open(sqlite.Open(databaseFile))
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	return db.WithContext(ctx)
+}
+
+func GetVersion() (*DbVersion, error) {
+	path := strings.Join(
+		[]string{"database", "versions.yml"},
+		string(os.PathSeparator),
+	)
+
+	f, err := os.ReadFile(path)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			panic(err)
+		}
+		return nil, err
+	}
+
+	var v DbVersion
+	if err := yaml.Unmarshal(f, &v); err != nil {
+		return nil, err
+	}
+
+	return &v, nil
+}
+
+func SetVersion(v DbVersion) error {
+	buf, err := yaml.Marshal(v)
+	if err != nil {
+		return err
+	}
+
+	msg := []byte("# This file is managed via scripts, don't edit\n\n")
+
+	msg = append(msg, buf...)
+	if err := os.WriteFile(versionFile, msg, 0666); err != nil {
+		panic(err)
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -4,12 +4,13 @@ go 1.20
 
 require (
 	github.com/go-chi/chi/v5 v5.0.8
+	github.com/go-chi/cors v1.2.1
+	gopkg.in/yaml.v3 v3.0.1
 	gorm.io/driver/sqlite v1.4.4
 	gorm.io/gorm v1.25.0
 )
 
 require (
-	github.com/go-chi/cors v1.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.15 // indirect
